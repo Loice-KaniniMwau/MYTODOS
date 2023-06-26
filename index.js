f73ff5d9971b68ece8cbd00da01fa78cece139c3
Loice-KaniniMwau <mwauloice@gmail.com>
2023-06-26 15:46:39 +0300

    changes to the todos

--- index.js
+++ index.js
@@ -1,98 +1,69 @@
-
-function fetchToDoList() {
-    fetch('https://dummyjson.com/todos')
-      .then(response => response.json())
-      .then(data => {
-        const tableBody = document.getElementById('todoTable').getElementsByTagName('tbody')[0];
-        tableBody.innerHTML = '';
-  
-        const dataArray = Array.isArray(data) ? data : [data]; 
-  
-        dataArray.forEach(item => {
-          const row = tableBody.insertRow();
-          row.innerHTML = `
-            <td>${item.id}</td>
-            <td>${item.title}</td>
-            <td>${item.completed}</td>
-            <td>
-              <button onclick="removeTask(${item.id})">Remove</button>
-              <button onclick="updateTask(${item.id})">Update</button>
-            </td>
-          `;
-        });
-      })
-      .catch(error => {
-        console.error('Error fetching the to-do list:', error);
     
-      });
-  }
-  
+let todos = [];
 
-  function addTask(userId,title, completed) {
-    const newTask = [{ userId,title, completed }];
-  
-    fetch('https://dummyjson.com/todos/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newTask)
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('New Task:', data);
-        fetchToDoList(); 
-      })
-      .catch(error => {
-        console.error('Error adding the task:', error);
-     
-      });
-  }
-  
-  function removeTask(taskId) {
-    fetch(`https://dummyjson.com/todos/${taskId}`, {
-      method: 'DELETE'
-    })
-      .then(response => {
-        if (response.ok) {
-          console.log('Task removed successfully');
-          fetchToDoList();
-        } else {
-          console.error('Error removing the task:', response.status);
-  
-        }
-      })
-      .catch(error => {
-        console.error('Error removing the task:', error);
-  
-      });
-  }
+const getToDos = () => {
+  return fetch('https://dummyjson.com/todos?limit=20')
+    .then(response => response.json())
+    .then(response => response)
+    .catch(error => error.message);
+};
 
-  function updateTask(taskId, updatedTask) {
-    fetch(`https://dummyjson.com/todos/${taskId}`, {
-      method: 'PUT', 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(updatedTask)
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Updated Task:', data);
-        fetchToDoList();
-      })
-      .catch(error => {
-        console.error('Error updating the task:', error);
-    
-      });
-  }
-  
- 
-  fetchToDoList();
-  
-  addTask(48,'Go to the mall',true);
-  
-  removeTask(1);
-  
-  updateTask(2, { title: 'Visit grandma', completed: false });
-  
+const displayToDos = () => {
+  const tableBody = document.querySelector('#todoTable tbody');
+  tableBody.innerHTML = ''; 
+
+  todos.forEach(item => {
+    const row = document.createElement('tr');
+    const idCell = document.createElement('td');
+    idCell.textContent = item.id;
+    const todoCell = document.createElement('td');
+    todoCell.textContent = item.todo;
+    const completedCell = document.createElement('td');
+    completedCell.textContent = item.completed;
+    const deleteCell = document.createElement('td'); 
+
+    const deleteButton = document.createElement('button'); 
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => deleteTask(item.id)); 
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(idCell);
+    row.appendChild(todoCell);
+    row.appendChild(completedCell);
+    row.appendChild(deleteCell);
+    tableBody.appendChild(row);
+  });
+};
+
+const addTask = event => {
+  event.preventDefault();
+
+  const newTaskTitle = document.getElementById('taskTitle').value;
+
+  const newTask = {
+    id: todos.length + 1,
+    todo: newTaskTitle,
+    completed: false
+  };
+
+  todos.push(newTask);
+
+  displayToDos();
+
+  document.getElementById('taskTitle').value = '';
+};
+
+const deleteTask = id => {
+  todos = todos.filter(item => item.id !== id); 
+  displayToDos();
+};
+
+document.getElementById('addTaskForm').addEventListener('submit', addTask);
+
+getToDos()
+  .then(response => {
+    todos = response.todos; 
+    displayToDos(); 
+  })
+  .catch(error => {
+    console.error('Error fetching todos:', error);
+  });
